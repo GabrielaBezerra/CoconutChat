afe8b52c21ba4e6b259ea38cc5928d17533a07f2
GabrielaBezerra <gabrieladecarvalhobezerra@gmail.com>
2020-08-08 20:48:57 -0300

    Initial Commit

---
+++ CoconutChat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoconutChat
+//
+//  Created by Gabriela Bezerra on 08/08/20.
+//  Copyright © 2020 Gabriela Bezerra. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
