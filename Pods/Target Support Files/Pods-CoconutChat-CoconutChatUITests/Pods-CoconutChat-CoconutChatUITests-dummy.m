c97a967247c8c6e590d62b8fcc2e264dd3dabc3e
GabrielaBezerra <gabrieladecarvalhobezerra@gmail.com>
2020-08-12 17:50:55 -0300

    add .gitignore and removing Pods files from git

--- Pods/Target Support Files/Pods-CoconutChat-CoconutChatUITests/Pods-CoconutChat-CoconutChatUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CoconutChat_CoconutChatUITests : NSObject
-@end
-@implementation PodsDummy_Pods_CoconutChat_CoconutChatUITests
-@end
--- Pods/Target Support Files/Pods-CoconutChatTests/Pods-CoconutChatTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CoconutChatTests : NSObject
-@end
-@implementation PodsDummy_Pods_CoconutChatTests
-@end
